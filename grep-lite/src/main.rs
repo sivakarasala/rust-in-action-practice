fcd38fd686f802578839d3b21bd3204944b91d64
SivaKrishna <shivataraya@gmail.com>
2022-11-03 18:24:12 +0530

    add clap for parsing command line args and regex for pattern match

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,15 +1,27 @@
-fn main() {
-    let search_term = "picture";
-    let quote = "\
-    Every face, every shop, bedroom window, public-house, and
-    dark square is a picture feverishly turned--in search of what?
-    It is the same with books.
-    What do we seek through millions of pages?";
+use regex::Regex;
+use clap::{App,Arg};
 
-    for (i, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            let line_num = i + 1;
-            println!("{}: {}", line_num, line);
+fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(Arg::with_name("pattern")
+            .help("The pattern to search for")
+            .takes_value(true)
+            .required(true))
+        .get_matches();
+    
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+    let quote = "Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books. What do we seek through millions of pages?";
+
+    for line in quote.lines() {
+        match re.find(line) {
+            Some(_) => println!("{}", line),
+            None => (),
         }
     }
 }
