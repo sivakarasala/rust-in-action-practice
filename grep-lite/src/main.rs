ec12307cee73e5b941cc865ebeb516116a4fbc67
SivaKrishna <shivataraya@gmail.com>
2022-11-03 18:36:48 +0530

    add file search capability along with stdin

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,6 +1,16 @@
 use regex::Regex;
 use clap::{App,Arg};
 
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
+
 fn main() {
     let args = App::new("grep-lite")
         .version("0.1")
@@ -9,19 +19,24 @@ fn main() {
             .help("The pattern to search for")
             .takes_value(true)
             .required(true))
+        .arg(Arg::with_name("input")
+            .help("File to search")
+            .takes_value(true)
+            .required(true))
         .get_matches();
     
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
+    let input = args.value_of("input").unwrap_or("-");
 
-    for line in quote.lines() {
-        match re.find(line) {
-            Some(_) => println!("{}", line),
-            None => (),
-        }
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
